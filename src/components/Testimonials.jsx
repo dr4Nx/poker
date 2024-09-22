237add03860d4d3529cf04998cefb9a6d9fb5572
Evan L <eluo2015@gmail.com>
2024-09-22 13:38:15 -0700

    Update Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -11,7 +11,7 @@ const Testimonials = () => {
                 <div key={index} className="w-full sm:w-1/2 lg:w-1/3 px-4 py-2">
                 <div className="bg-neutral rounded-md p-6 text-md border border-neutral-800 font-thin">
                     <p>{testimonial.text}</p>
-                    <div className="flext mt-8 items-start">
+                    <div className="flex mt-8 items-start">
                         <img className="w-12 h-12 mr-6 rounded-full border border-neutral-300" src={testimonial.image} alt={testimonial.user}/>
                         <div>
                             <h6>{testimonial.user}</h6>
